a6bdc55fe53238e8cb2ae7d5ed4428f526917891
keisuke-nao-baseball <matsukei7539@gmail.com>
2021-07-02 11:13:46 +0900

    実装開始

---
+++ api/src/main/java/com/score/basaball/BasaballApplication.java
@@ -0,0 +1,13 @@
+package com.score.basaball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasaballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasaballApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/score/basaball/BasaballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.score.basaball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasaballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
